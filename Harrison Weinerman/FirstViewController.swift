fc4841ff0018cbcb4b7ad420bcb36d886f25670c
Harrison Weinerman <h@rrison.co>
2016-11-15 13:38:52 -0500

    init

---
+++ Harrison Weinerman/FirstViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FirstViewController.swift
+//  Harrison Weinerman
+//
+//  Created by Harrison Weinerman on 4/19/16.
+//  Copyright © 2016 harrisonweinerman. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
